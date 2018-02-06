b9b41662b5b5edb695c0f32450ea69a868e15c26
erichstuder <richu_@hotmail.com>
2018-02-06 22:20:09 +0100

    2018_02_06b

--- Octave/designHelpers/optimizeRelTol.m
+++ Octave/designHelpers/optimizeRelTol.m
@@ -1,24 +1,25 @@
 function relTolOptimized = optimizeRelTol(maxData, absTol, relTol)
     targetValue = directMethod_encode(maxData, absTol, relTol, 0);
-    relTolOptimized = directMethod_calcRelTol(maxData, targetValue + 0.5 - 3*eps(targetValue), absTol, 0);
-%    
-%%    relTolOptimizedBase = relTolOptimized;
-%    while(targetValue < directMethod_encode(maxData, absTol, relTolOptimized, 0))
-%      relTolOptimized = relTolOptimized - eps(relTolOptimized);
-%      %pause(0.001);
-%%      disp(relTolOptimizedBase - relTolOptimized)
-%%      disp(directMethod_encode(maxData, absTol, relTolOptimized, 0))
-%%      disp(targetValue)
-%    end
-%    
-%    relTolOptimized_old = relTolOptimized;
-%    relTolOptimized = relTolOptimized + eps(relTolOptimized);
-%    while(targetValue >= directMethod_encode(maxData, absTol, relTolOptimized, 0))
-%      relTolOptimized_old = relTolOptimized;
-%      relTolOptimized = relTolOptimized + eps(relTolOptimized);
-%      %disp(relTolOptimized);
-%    end
-%    
-%    relTolOptimized = relTolOptimized_old;
+    relTolOptimized = directMethod_calcRelTol(maxData, targetValue + 0.5, absTol, 0);
+    
+%    relTolOptimizedBase = relTolOptimized;
+    while(targetValue >= directMethod_encode(maxData, absTol, relTolOptimized, 0))
+      relTolOptimized = relTolOptimized - eps(relTolOptimized);
+      %pause(0.001);
+%      disp(relTolOptimizedBase - relTolOptimized)
+%      disp(directMethod_encode(maxData, absTol, relTolOptimized, 0))
+%      disp(targetValue)
+    end
+    
+    %relTolOptimized_old = relTolOptimized;
+    %relTolOptimized = relTolOptimized + eps(relTolOptimized);
+    while(targetValue < directMethod_encode(maxData, absTol, relTolOptimized, 0))
+      %relTolOptimized_old = relTolOptimized;
+      relTolOptimized = relTolOptimized + eps(relTolOptimized);
+      %disp(relTolOptimized);
+      %disp(directMethod_encode(maxData, absTol, relTolOptimized, 0))
+    end
+    
+    %relTolOptimized = relTolOptimized_old;
     
     
